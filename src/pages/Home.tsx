5151595447bed7c73308686b1430bff231e9a0da
Eme <contactaconmiriam@gmail.com>
2022-04-07 19:58:15 +0200

    add: initial call to API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import getArtWorkService from '../services/artworks/getArtWorkService'
 
 const Home = () => {
-  // eslint-disable-next-line  no-unused-vars
-  const [imgUrl, setImgUrl] = useState('')
-  const navigate = useNavigate()
   let artworks = []
+  const [imgUrl, setImgUrl] = useState('')
   // eslint-disable-next-line  no-unused-vars
-
-  const apiCall = () => {
-    getArtWorkService().then(res => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    getArtWorkService().then((res) => {
       artworks = [...res]
       console.log(artworks, 'heeeeeeey')
-      setImgUrl(`${process.env.REACT_APP_HOST_IMAGES_API}/${res[0].imageId}/full/843,/0/default.jpg`)
+      setImgUrl(
+        `${process.env.REACT_APP_HOST_IMAGES_API}/${res[0].imageId}/full/843,/0/default.jpg`
+      )
       // imgUrl ? console.log(imgUrl) : ''
     })
-  }
+  })
+  // eslint-disable-next-line  no-unused-vars
 
   return (
     <div>
       <h1>Heeeeyyyy Home</h1>
       <button onClick={() => navigate('/details')}>Ir a details</button>
-      <button onClick={() => apiCall()}>get artworks</button>
-      <div>
-        {imgUrl && <img src={imgUrl} alt="cositas" />}
-      </div>
+      <div>{imgUrl && <img src={imgUrl} alt='cositas' />}</div>
     </div>
   )
 }
