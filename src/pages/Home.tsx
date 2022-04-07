945fec4fe002807e97860767b485f35884727d7d
Eme <contactaconmiriam@gmail.com>
2022-04-07 19:36:22 +0200

    add: add image from api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router'
 import getArtWorkService from '../services/artworks/getArtWorkService'
 
 const Home = () => {
+  // eslint-disable-next-line  no-unused-vars
+  const [imgUrl, setImgUrl] = useState('')
   const navigate = useNavigate()
+  let artworks = []
+  // eslint-disable-next-line  no-unused-vars
+
   const apiCall = () => {
-    console.log(getArtWorkService())
+    getArtWorkService().then(res => {
+      artworks = [...res]
+      console.log(artworks, 'heeeeeeey')
+      setImgUrl(`${process.env.REACT_APP_HOST_IMAGES_API}/${res[0].imageId}/full/843,/0/default.jpg`)
+      // imgUrl ? console.log(imgUrl) : ''
+    })
   }
 
   return (
     <div>
       <h1>Heeeeyyyy Home</h1>
-
       <button onClick={() => navigate('/details')}>Ir a details</button>
       <button onClick={() => apiCall()}>get artworks</button>
+      <div>
+        {imgUrl && <img src={imgUrl} alt="cositas" />}
+      </div>
     </div>
   )
 }
