1c645d1609339ad39a9ea412b4bb310e1df25023
Ale <alehaa19@gmail.com>
2022-04-11 20:18:29 +0200

    add: back to top button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,19 @@ import { useNavigate } from 'react-router'
 import getArtWorkService from '../services/artworks/getArtWorkService'
 import CardsGrid from '../components/CardsGrid'
 import Box from '@mui/material/Box'
+import BackToTop from '../components/BackToTop'
+import { ArtWorkMappedItem } from '../services/artworks/model'
 export interface HomeProps {
   page: number
 }
 const Home = () => {
-  const [artworks, setArtworks] = useState([])
+  const [artworks, setArtworks] = useState<ArtWorkMappedItem[]>([])
   const navigate = useNavigate()
   let page = 1
 
   useEffect(() => {
     getArtWorkService(page).then((res) => {
-      setArtworks(res as any)
+      setArtworks(res)
     })
   }, [])
 
@@ -23,7 +25,7 @@ const Home = () => {
       if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) {
         return getArtWorkService(page + 1).then((res) => {
           page = page + 1
-          setArtworks(prev => [...prev, ...res] as any)
+          setArtworks(prev => [...prev, ...res])
           window.addEventListener('scroll', scrollingFunction)
         })
       }
@@ -37,6 +39,7 @@ const Home = () => {
       <button onClick={() => navigate('/details')}>Ir a details</button>
       <Box sx={{ mt: 5 }}>
         {artworks.length > 0 ? <CardsGrid artworkList={artworks} /> : ''}
+        <BackToTop window={() => window} />
       </Box>
     </div>
   )
